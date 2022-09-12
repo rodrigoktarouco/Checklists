043f8aef83c8ce22cf8943770c60017f9ab3a4b7
Rodrigo Kroef tarouco <rodrigoktarouco@gmail.com>
2022-09-12 08:55:35 -0300

    Basic setup, and gitignore file added to the project

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Rodrigo Kroef Tarouco on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
