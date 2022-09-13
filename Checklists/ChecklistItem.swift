835a1a1bb09bc1049c31219f6a17a0ad49b75889
Rodrigo Kroef tarouco <rodrigoktarouco@gmail.com>
2022-09-13 09:10:09 -0300

    Refactoring and bugfix

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Rodrigo Kroef Tarouco on 13/09/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
